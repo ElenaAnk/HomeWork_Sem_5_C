20869ccb7bb0c033a1356240907b0dd478af575e
Elena <elena_nn@list.ru>
2023-06-25 00:39:24 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных ИНДЕКСАХ.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int [] GetArray(int size, int minValue, int maxValue)
+{
+    int[]result = new int [size];
+    for(int i = 0; i<size; i++)
+    {
+        result[i]=new Random().Next(minValue,maxValue+1);
+    }
+    return result;
+}
+int [] array = GetArray(10,-100,100);
+Console.WriteLine($"[{String.Join(",",array)}]");
+
+int sumNegative = 0;
+int size = array.Length;
+for (int i =0; i< size; i++)
+{
+    if(i%2!=0)
+    {
+        sumNegative+=array[i];
+    }
+    else
+    {
+        sumNegative+=0;
+    }
+}
+
+Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах = {sumNegative}");
+
