818576e3fc2d80d737eaa6fef1fad4b74f536e83
Elena <elena_nn@list.ru>
2023-06-25 00:41:44 +0300

    Task2_

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,29 +2,29 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
-int [] GetArray(int size, int minValue, int maxValue)
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    int[]result = new int [size];
-    for(int i = 0; i<size; i++)
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        result[i]=new Random().Next(minValue,maxValue+1);
+        result[i] = new Random().Next(minValue, maxValue + 1);
     }
     return result;
 }
-int [] array = GetArray(10,-100,100);
-Console.WriteLine($"[{String.Join(",",array)}]");
+int[] array = GetArray(10, -100, 100);
+Console.WriteLine($"[{String.Join(",", array)}]");
 
 int sumNegative = 0;
 int size = array.Length;
-for (int i =0; i< size; i++)
+for (int i = 0; i < size; i++)
 {
-    if(i%2!=0)
+    if (i % 2 != 0)
     {
-        sumNegative+=array[i];
+        sumNegative += array[i];
     }
     else
     {
-        sumNegative+=0;
+        sumNegative += 0;
     }
 }
 
