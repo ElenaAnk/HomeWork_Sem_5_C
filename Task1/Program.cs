b112f014ebce11ac2d3b3b9d5a278608f257b7db
Elena <elena_nn@list.ru>
2023-06-25 00:41:15 +0300

    Task1_

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,19 +6,19 @@ int[] GetArray(int size, int minValue, int maxValue)
     int[] result = new int[size];
     for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(minValue, maxValue+1);
+        result[i] = new Random().Next(minValue, maxValue + 1);
     }
     return result;
 }
 
-int[]array = GetArray(10,100,999);
-Console.WriteLine($"[{String.Join(",",array)}]");
+int[] array = GetArray(10, 100, 999);
+Console.WriteLine($"[{String.Join(",", array)}]");
 
 int count = 0;
 
 foreach (int el in array)
 {
-    count+= el%2==0?1:0;
+    count += el % 2 == 0 ? 1 : 0;
 }
 Console.WriteLine($"Количество чётных числе в данном массиве = {count}");
 
